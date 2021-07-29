b13cc14aba53a52d9026bbf0ca06fbf0f87e3ace
Bojan Janevski <Bojan.Janevski@endava.com>
2021-07-29 10:29:23 +0200

    Initial Commit

---
+++ src/test/java/com/example/springcorehomework/SpringCoreHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcorehomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
