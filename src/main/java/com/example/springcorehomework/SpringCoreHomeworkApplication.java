98b16a3f33544d81617019eaa35d793fc89f49eb
Bojan Janevski <Bojan.Janevski@endava.com>
2021-08-02 20:21:29 +0200

    Add database records

--- src/main/java/com/example/springcorehomework/SpringCoreHomeworkApplication.java
+++ src/main/java/com/example/springcorehomework/SpringCoreHomeworkApplication.java
@@ -1,13 +1,25 @@
 package com.example.springcorehomework;
 
+import model.Author;
+import model.Book;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class SpringCoreHomeworkApplication {
+public class SpringCoreHomeworkApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCoreHomeworkApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Author author = Author.builder().name("Stephen King").build();
+        Book book1 = Book.builder().title("The Stand").build();
+        Book book2 = Book.builder().title("The Shining").build();
+        author.setBooks(List.of(book1, book2));
+    }
 }
